eb2a5b8cbe78526e22636485eab7031f28ff3fd4
Lucas <lucasbcmoraes@gmail.com>
2021-07-10 19:34:07 -0300

    funcao que verifica se alguem ganhou

--- script.js
+++ script.js
@@ -22,7 +22,35 @@ const Tabuleiro = (() => {
     ],
   }
 
-  const verificaVitoria = () => {}
+  const verificaVitoria = () => {
+    const listaVitorias = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      //horizontal
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      //diagonal
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+
+    let { casas } = estados
+
+    for (let [a, b, c] of listaVitorias) {
+      if (
+        casas[a].sinal === casas[b].sinal &&
+        casas[a].sinal === casas[c].sinal &&
+        casas[a].sinal != undefined
+      ) {
+        return alert(`${casas[a].sinal} ganhou!`)
+      }
+    }
+    if (casas.every((cadaCasa) => cadaCasa.sinal != undefined)) {
+      return alert('EMPATE')
+    }
+  }
 
   tabuleiroH.forEach((casadiv, casaindice) => {
     casadiv.addEventListener('click', () => {
@@ -34,6 +62,7 @@ const Tabuleiro = (() => {
         casa.marcado = !casa.marcado
         casa.sinal = jogadorDaVez
         estados.turno = !estados.turno
+        verificaVitoria()
       }
     })
   })
